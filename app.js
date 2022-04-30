63e1bf020dceb0f9e756194aa685731453d789cd
lucasps99 <lucaspisa99@gmail.com>
2022-04-30 13:40:10 +0200

    isgame ready

--- app.js
+++ app.js
@@ -2,8 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+let waitingList = ['lucas', 'lucas2']
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/game', (req, res) => {
+  isgameready = false;  
+  if(waitingList.length > 1) { 
+    isgameready = true;
+    waitingList.keys.toString().
+    let user1 = waitingList.pop();
+    let user2 = waitingList.pop();
+  }
+  res.json({'isgameready':isgameready});
 })
 
 app.listen(port, () => {
