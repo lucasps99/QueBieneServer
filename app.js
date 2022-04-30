76135fdac81b3a7cc5613c6e993b76f313ac4532
lucasps99 <lucaspisa99@gmail.com>
2022-05-01 00:44:45 +0200

    cors

--- app.js
+++ app.js
@@ -5,12 +5,7 @@ app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use( cors( { origin: true } ) );
 
 let roomList = new Map();
 
