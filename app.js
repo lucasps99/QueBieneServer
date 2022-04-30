241bdb7363d80c13aa747f7a717381b5185c7a3e
ivgasa99 <ivgasa99@gmail.com>
2022-04-30 13:16:09 +0200

    post game

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+var waitingList = [];
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/game', (req, res) => {
+  const userId = req.get("userId");
+  if (userId == undefined) {
+    res.sendStatus(400);
+  }
+  else {
+    waitingList.push(userId);
+    console.log('User ' + userId + ' added to userQueue')
+    console.log(waitingList)
+    res.sendStatus(200);
+  }
+  
 })
 
 app.listen(port, () => {
