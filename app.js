ed161083aabbfdcefdca9ee408aa498b7fe3f193
lucasps99 <lucaspisa99@gmail.com>
2022-05-01 00:22:03 +0200

    add cors

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const cors = require("cors");
+app.use(cors());
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 let roomList = new Map();
 
